5c2b817becf9bbb06913b6ccf959ee3e18e66b13
rahul9230 <rahulkasturi92300@gmail.com>
2020-08-21 18:03:19 +0530

    Final Commit

---
+++ omts-booking-ms/src/main/java/com/capg/omts/booking/controller/BookingMovieController.java
@@ -0,0 +1,43 @@
+package com.capg.omts.booking.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.capg.omts.booking.model.Payment;
+import com.capg.omts.booking.model.Seat;
+import com.capg.omts.booking.service.*;
+
+@CrossOrigin("https://localhost:4200")
+@RestController
+@RequestMapping("/booking")
+public class BookingMovieController {
+	@Autowired
+	BookingServiceImpl service;
+		
+	@GetMapping("/seats")	
+	public ResponseEntity<List<Seat>> calculateTotalCost(@RequestBody List<Seat> seats){
+		service.calculateTotalCost(seats);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	@PutMapping("/choose/seats")	
+	public ResponseEntity<List<Seat>> chooseSeats(@RequestBody List<Integer> seatIds){
+	
+		return new ResponseEntity<>(service.chooseSeats(seatIds),HttpStatus.OK);
+	}
+	
+	@PostMapping("/payment")
+	public ResponseEntity<Payment> choosePaymentMethod(Payment payment) {
+		return new ResponseEntity<>(service.choosePaymentMethod(payment),HttpStatus.OK);
+		
+	}	
+
+}
---
+++ omts-booking-ms/src/main/java/com/capg/omts/booking/service/SeatServiceImpl.java
@@ -0,0 +1,31 @@
+package com.capg.omts.booking.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import com.capg.omts.booking.model.Seat;
+
+import com.capg.omts.booking.repository.SeatRepository;
+
+@Service
+public class SeatServiceImpl implements SeatService {
+
+	@Autowired
+	SeatRepository seatRepo;
+	@Autowired
+	RestTemplate rt;
+	
+	@Override
+	public Seat addSeat(Seat seat) {
+		
+		return rt.postForObject("http://seat-ms/seat/add", seat, Seat.class);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Seat> getAvailableSeats() {
+		
+		return (List<Seat>) rt.getForObject("http://seat-ms/seat/all", Seat.class);
+	}
+}
